1d97056f566f5cf66c16305e442b4e2eeb200f62
Ben Hale <bhale@pivotal.io>
2017-06-20 12:13:43 -0700

    [ci skip] Incremental commit. Please do not base any external work off of this commit.

--- lib/java_buildpack/component/base_component.rb
+++ lib/java_buildpack/component/base_component.rb
@@ -16,6 +16,7 @@
 require 'fileutils'
 require 'java_buildpack/component'
 require 'java_buildpack/util/cache/application_cache'
+require 'java_buildpack/util/colorize'
 require 'java_buildpack/util/format_duration'
 require 'java_buildpack/util/shell'
 require 'java_buildpack/util/space_case'
@@ -86,10 +87,10 @@ module JavaBuildpack
       # @return [Void]
       def download(version, uri, name = @component_name)
         download_start_time = Time.now
-        print "-----> Downloading #{name} #{version} from #{uri.sanitize_uri} "
+        print "#{'----->'.cyan.bold} Downloading #{name} #{version} from #{uri.sanitize_uri} "
 
         JavaBuildpack::Util::Cache::ApplicationCache.new.get(uri) do |file, downloaded|
-          puts downloaded ? "(#{(Time.now - download_start_time).duration})" : '(found in cache)'
+          puts downloaded ? "(#{(Time.now - download_start_time).duration})" : '(found in cache)'.green
           yield file
         end
       end
---
+++ lib/java_buildpack/util/colorize.rb
@@ -0,0 +1,108 @@
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2017 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class String
+
+  def bg_black
+    tty? "\e[40m#{self}\e[0m"
+  end
+
+  def bg_blue
+    tty? "\e[44m#{self}\e[0m"
+  end
+
+  def bg_brown
+    tty? "\e[43m#{self}\e[0m"
+  end
+
+  def bg_cyan
+    tty? "\e[46m#{self}\e[0m"
+  end
+
+  def bg_gray
+    tty? "\e[47m#{self}\e[0m"
+  end
+
+  def bg_green
+    tty? "\e[42m#{self}\e[0m"
+  end
+
+  def bg_magenta
+    tty? "\e[45m#{self}\e[0m"
+  end
+
+  def bg_red
+    tty? "\e[41m#{self}\e[0m"
+  end
+
+  def black
+    tty? "\e[30m#{self}\e[0m"
+  end
+
+  def blink
+    tty? "\e[5m#{self}\e[25m"
+  end
+
+  def blue
+    tty? "\e[34m#{self}\e[0m"
+  end
+
+  def bold
+    tty? "\e[1m#{self}\e[22m"
+  end
+
+  def brown
+    tty? "\e[33m#{self}\e[0m"
+  end
+
+  def cyan
+    tty? "\e[36m#{self}\e[0m"
+  end
+
+  def gray
+    tty? "\e[37m#{self}\e[0m"
+  end
+
+  def green
+    tty? "\e[32m#{self}\e[0m"
+  end
+
+  def italic
+    tty? "\e[3m#{self}\e[23m"
+  end
+
+  def magenta
+    tty? "\e[35m#{self}\e[0m"
+  end
+
+  def red
+    tty? "\e[31m#{self}\e[0m"
+  end
+
+  def reverse_color
+    tty? "\e[7m#{self}\e[27m"
+  end
+
+  def underline
+    tty? "\e[4m#{self}\e[24m"
+  end
+
+  private
+
+  def tty?(value)
+    $stdout.tty? ? value : self
+  end
+
+end
